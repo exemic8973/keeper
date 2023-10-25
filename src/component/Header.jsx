5dab6093d927ab479416eb7becebbd56b79dbd34
Eric Li <wei8973@gmail.com>
2023-10-25 11:12:13 -0500

    first Note App commit

---
+++ src/component/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const appName = "Keeper";
+
+function Header() {
+  return (
+    <div className="headerBox">
+      <header>
+        <h1>{appName}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
